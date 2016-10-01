63e2ed0234ba5320b03b9e4028b8d21a2881755a
杉山佑里子 <peaceful.future1101@gmail.com>
2016-10-01 20:07:36 +0900

    contact fix

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,8 @@ class Contact < ApplicationRecord
 #  attr_accessor :name, :email, :message
  
   validates :name, presence:true
-  validates :email, presence:true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,  format: { with: VALID_EMAIL_REGEX }
   validates :message, presence:true
   
 
